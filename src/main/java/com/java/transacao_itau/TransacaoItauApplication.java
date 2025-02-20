c8dce390844c5ddc85c319b555056aa569f09a79
Eduardo Araújo <eduardobfm5@gmail.com>
2025-02-19 22:48:06 -0300

    first commit

---
+++ src/main/java/com/java/transacao_itau/TransacaoItauApplication.java
@@ -0,0 +1,13 @@
+package com.java.transacao_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/transacao_itau/TransacaoItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.transacao_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
